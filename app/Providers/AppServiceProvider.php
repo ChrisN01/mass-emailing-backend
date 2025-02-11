5b805faf4a13b185e60e04c0d16bd4210f1da4a4
Crisleivys Gil <crisleivysngil@gmail.com>
2025-02-11 09:57:34 -0400

    added the CsvValidator to AppProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,8 @@ use App\Repositories\Contracts\CsvRepositoryInterface;
 use App\Repositories\CsvRepository;
 use App\Services\Contracts\CsvProcessorInterface;
 use App\Services\CsvProcessor;
+use App\Validators\Contracts\CsvValidatorInterface;
+use App\Validators\CsvValidator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -17,6 +19,7 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->app->bind(CsvProcessorInterface::class, CsvProcessor::class);
         $this->app->bind(CsvRepositoryInterface::class, CsvRepository::class);
+        $this->app->bind(CsvValidatorInterface::class, CsvValidator::class);
     }
 
     /**
